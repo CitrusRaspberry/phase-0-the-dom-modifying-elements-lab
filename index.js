f5ae06d63a2b64c06d4284d7229377e05d15a6f4
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-10-23 19:49:57 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const body = document.body;
+
+const main = document.querySelector("main#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Josh is the champion";
+body.append(newHeader);
